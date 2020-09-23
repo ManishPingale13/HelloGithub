709f941179f62f269b45a45c382ccf4846deddb7
Manish Pingale <manishpingale2005@gmail.com>
2020-09-23 15:31:07 +0530

    First Time Committing From Android Studio

---
+++ app/src/main/java/com/example/firstkotlinmsgshare/Model.kt
@@ -0,0 +1,21 @@
+package com.example.firstkotlinmsgshare
+
+data class Hobby(var title: String)
+
+object Supplier {
+
+    val hobbies = listOf(
+        Hobby("Reading"),
+        Hobby("Swimming"),
+        Hobby("Coding"),
+        Hobby("Gaming"),
+        Hobby("Reading"),
+        Hobby("Swimming"),
+        Hobby("Coding"),
+        Hobby("Gaming"),
+        Hobby("Reading"),
+        Hobby("Swimming"),
+        Hobby("Coding"),
+        Hobby("Gaming")
+    )
+}
